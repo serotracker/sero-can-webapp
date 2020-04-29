cdaec774d8bc592e0c6fef0a5c776ff6ed75058c
Ewan May <ewan.may@ucalgary.ca>
2020-04-28 23:49:43 -0600

    Initial commit for map functionality

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { AppContextProvider } from "./context";
 import Component from "./components/Component"
+import Map from "./components/map/Map";
 
 function App() {
   return (
     <AppContextProvider>
       <Component></Component>
+      <Map></Map>
     </AppContextProvider>
   );
 }
