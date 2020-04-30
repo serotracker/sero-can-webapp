771a193496f8d5c28538050919a0fff51514c6a3
Austin Atmaja <austin.atmaja@gmail.com>
2020-04-29 20:59:17 -0400

    Add functions to get aggregated data by some aggregation_factor

--- react-app/src/metaAnalysis.ts
+++ react-app/src/metaAnalysis.ts
@@ -9,7 +9,7 @@ function calcPooledPrevalence(records: AirtableRecord[]) {
 
     records.forEach((record: AirtableRecord) => {
         if ((record.seroprevalence !== null) && (record.denominator !== null)) {
-            const variance: number = (record.seroprevalence*(1 - record.seroprevalence)) / record.denominator;
+            const variance = (record.seroprevalence*(1 - record.seroprevalence)) / record.denominator;
             var_sum = var_sum + variance;
             inv_var_sum = inv_var_sum + (1/variance);
             p_over_var_sum = p_over_var_sum + (record.seroprevalence/variance); 
@@ -20,4 +20,30 @@ function calcPooledPrevalence(records: AirtableRecord[]) {
         seroprevalence: p_over_var_sum / inv_var_sum,
         error: Z_SCORE*Math.sqrt(var_sum)
     } 
+}
+
+// Given an aggregation factor (either 'countries' or 'populations')
+// get pooled seroprevalence and error bounds for each country or population
+export function getAggregateData(records: AirtableRecord[], aggregation_factor: "countries" | "populations") {
+    const grouped_records: Record<string, AirtableRecord[]> = {}
+    records.forEach((record: AirtableRecord) => {
+        if ((record.seroprevalence !== null) && (record.denominator !== null) && (record[aggregation_factor] !== null)) {
+            record[aggregation_factor]!.forEach((entry: string) => {
+                if (entry in grouped_records){
+                    grouped_records[entry].push(record);
+                }
+                else {
+                    grouped_records[entry] = [record];
+                }
+            });
+        }
+    });
+
+    const aggregate_data: Record<string, string | number>[] = []
+
+    for(const name in grouped_records){
+        const result = calcPooledPrevalence(grouped_records[name]);
+        aggregate_data.push({ ...result, name });
+    }
+    return aggregate_data;
 }
