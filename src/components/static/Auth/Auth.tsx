c850038ced8e3351f3071de21ca3c5ead2f89162
Ewan May <ewan.may@ucalgary.ca>
2020-05-03 18:19:12 -0600

    new tabs and card for password input, as well as icons

--- src/components/static/Auth/Auth.tsx
+++ src/components/static/Auth/Auth.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
-import { Input, Button } from 'semantic-ui-react'
+import { Input, Button, Card } from 'semantic-ui-react'
 import { checkPassword } from "./AuthController"
-
+import Img from '../../../assets/images/two-tone-dark.svg'
 export default function Auth(props: Record<string, any>) {
     const [passEntry, setPassEntry] = useState('');
 
@@ -23,21 +23,25 @@ export default function Auth(props: Record<string, any>) {
 
     return (
         <div className="col-12 p-0 flex center-item">
-            <h2 className="col-12 p-0 center-item flex">
-                SeroTracker
-            </h2>
-            <form onSubmit={handleSubmit} className="input-group col-4 p-0 center-item flex">
-                <Input
-                    focus
-                    type="password"
-                    onChange={handleChange}
-                    value={passEntry}
-                    className="col"
-                    placeholder="Passphrase" />
-                <Button type="submit" content="Submit" 
-                    className="col-auto" primary>
-                </Button>
-            </form>
+            <Card className="col-4">
+                <Card.Header className="flex p-3">
+                <img src={Img} width={30} height={30} alt="React Logo" />
+                <h2 className="col-auto m-0 flex">SeroTracker</h2></Card.Header>
+                <Card.Content>
+                        <form onSubmit={handleSubmit} className="input-group col-12 p-0 center-item flex">
+                            <Input
+                                focus
+                                type="password"
+                                onChange={handleChange}
+                                value={passEntry}
+                                className="col"
+                                placeholder="Passphrase" />
+                            <Button type="submit" content="Submit"
+                                className="col-auto" primary>
+                            </Button>
+                        </form>
+                </Card.Content>
+            </Card>
         </div>
     )
 }
