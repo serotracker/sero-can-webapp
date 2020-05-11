032a5f82c7d0a7aeaa4612bcda5a20c3e6d05a17
Ewan May <ewan.may@ucalgary.ca>
2020-05-11 17:53:10 -0600

    fix nav bar responsiveness issues

--- src/App.tsx
+++ src/App.tsx
@@ -73,7 +73,7 @@ function App() {
                   <div className="col-auto px-2" >SeroTracker</div>
                 </Link>
               </div>
-              <div className="App-tabs col-2">
+              <div className="App-tabs col-sm-4 col-lg-2">
                 <Link className={getTabClass('/Home')} to="/Home">Home</Link>
                 <Link className={getTabClass('/Data')} to="/Data">Data</Link>
                 <Link className={getTabClass('/About')} to="/About">About</Link>
