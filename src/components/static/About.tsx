e95b0dd2876d9fc7ee49424d9d6abb8b77b51151
Rahul Arora <62099239+rahularoradfs@users.noreply.github.com>
2020-05-23 01:00:01 -0600

    team delta

--- src/components/static/About.tsx
+++ src/components/static/About.tsx
@@ -82,8 +82,8 @@ export default function About() {
                     {renderBioBlock('Tingting Yan', ['University of Toronto'])}
                     {renderBioBlock('Christian Cao', ['University of Calgary'])}
                     {renderBioBlock('Claire Donnici', ['University of Calgary'])}
+                    {renderBioBlock('Natasha Ilincic', ['University of Guelph'])}
                     {renderBioBlock('Michael Liu', ['University of Oxford', 'Harvard University'])}
-                    {renderBioBlock('Brianna Rosgen', ['University of Calgary'])}
                     {renderBioBlock('Mitchell Segal', ['University of Toronto'])}
                 </div>
                 <h2 className="team-name-text">
