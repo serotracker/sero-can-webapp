dac1065f1c956d2e7538ccd985171b57fce24924
Ewan May <ewan.may@arcurve.com>
2020-08-05 10:46:10 -0600

    sidebar update

--- src/components/sidebar/left-sidebar/LeftSidebar.tsx
+++ src/components/sidebar/left-sidebar/LeftSidebar.tsx
@@ -1,9 +1,8 @@
 import React from "react";
+import "../sidebar.css";
+import AnalysisMethods from "./AnalysisMethods";
 import CountryList from "./CountryList";
 import TotalStats from "./TotalStats";
-import AnalysisMethods from "./AnalysisMethods";
-import "../sidebar.css"
-import Translate from "../../../utils/translate/translateService";
 
 export default function LeftSidebar() {
   return (
@@ -11,19 +10,6 @@ export default function LeftSidebar() {
       <TotalStats/>
       <AnalysisMethods/>
       <CountryList/>
-      <div className="col-12 pb-1 fit">
-        <div className='col-12 p-0 center-item flex'>
-          <div className="section-title">
-            <i>{Translate("LancetArticle").toUpperCase()}</i>{Translate("Article", null, null, [true, false]).toUpperCase()}
-          </div>
-        </div>
-        <div className="py-1 center"><a
-          className="p-1 flex center-item"
-          target="__blank"
-          rel="noopener noreferrer"
-          href="https://www.thelancet.com/journals/laninf/article/PIIS1473-3099(20)30631-9/fulltext#%20">{Translate('ViewHere')}</a>
-        </div>
-      </div>
     </div>
   )
 }
