54dff2d0263b751d10241fb883d81108cd93a62d
Austin Atmaja <austin.atmaja@gmail.com>
2020-08-05 09:54:37 -0400

    Wording touchups, analysis tab resizing, etc

--- src/components/sidebar/right-sidebar/LastUpdated.tsx
+++ src/components/sidebar/right-sidebar/LastUpdated.tsx
@@ -10,18 +10,8 @@ export default function LastUpdated() {
   return (
     <div className="col-12 pb-2">
       <div className='col-12 p-0 center-item flex'>
-        <div className="section-title">
-        {Translate("LastUpdated").toUpperCase()}
-        </div>
-        <div className="tooltip-vert-adj">
-          <InformationIcon
-              offset={10}
-              position="top right"
-              color="#455a64"
-              tooltipHeader={Translate("LastUpdated")}
-              popupSize="small"
-              size="sm"
-              tooltip={Translate("LastUpdatedTooltip")}/>
+        <div className="section-title center last-refreshed-title">
+        {Translate("ServerLastRefreshed").toUpperCase()}
         </div>
       </div>
       <div className="py-2 center">
