a0b6b9a349e66e84c22d98d522374324ffbb407a
Brett <brettdziedzic@gmail.com>
2021-01-16 23:23:44 -0700

    plotting PBF response :)

--- src/components/map/MapboxMap.tsx
+++ src/components/map/MapboxMap.tsx
@@ -11,25 +11,28 @@ interface state {
     zoom: number
   }
 
+// Takes in the style JSON object from VectorTileServer API response 
+// Modifies & adds attributes for Mapbox GL compatability 
 function format (style : any, metadata : any, endpointUrl : any) {
+
+style.sprite = "https://tiles.arcgis.com/tiles/5T5nSi527N4F7luB/arcgis/rest/services/WHO_Polygon_Basemap_no_labels/VectorTileServer/resources/styles/../sprites/sprite";
+style.glyphs = "https://tiles.arcgis.com/tiles/5T5nSi527N4F7luB/arcgis/rest/services/WHO_Polygon_Basemap_no_labels/VectorTileServer/resources/styles/../fonts/{fontstack}/{range}.pbf";
 // ArcGIS Pro published vector services dont prepend tile or tileMap urls with a /
 style.sources.esri = {
     type: 'vector',
-    scheme: 'xyz',
-    tilejson: metadata.tilejson || '2.0.0',
-    format: (metadata.tileInfo && metadata.tileInfo.format) || 'pbf',
+    tiles: ["https://tiles.arcgis.com/tiles/5T5nSi527N4F7luB/arcgis/rest/services/WHO_Polygon_Basemap_Disputed_Areas_and_Borders_VTP/VectorTileServer/tile/{z}/{y}/{x}.pbf"],
+    maxzoom: 23,  
+    //scheme: 'xyz',
+    //tilejson: metadata.tilejson || '2.0.0',
+    //format: (metadata.tileInfo && metadata.tileInfo.format) || 'pbf',
     /* mapbox-gl-js does not respect the indexing of esri tiles
     because we cache to different zoom levels depending on feature density, in rural areas 404s will still be encountered.
     more info: https://github.com/mapbox/mapbox-gl-js/pull/1377
     */
-    index: metadata.tileMap ? style.sources.esri.url + '/' + metadata.tileMap : null,
-    maxzoom: 15,  
-    tiles: [
-        //style.sources.esri.url + '/' + metadata.tiles[0]
-        "https://tiles.arcgis.com/tiles/5T5nSi527N4F7luB/arcgis/rest/services/WHO_Polygon_Basemap_Disputed_Areas_and_Borders_VTP/VectorTileServer/tile/{z}/{y}/{x}.pbf"
-    ],
-    description: metadata.description,
-    name: metadata.name
+    //index: metadata.tileMap ? style.sources.esri.url + '/' + metadata.tileMap : null,
+    //maxzoom: 23,  
+    //url: "https://tiles.arcgis.com/tiles/5T5nSi527N4F7luB/arcgis/rest/services/WHO_Polygon_Basemap_Disputed_Areas_and_Borders_VTP/VectorTileServer/tile/{z}/{y}/{x}.pbf",
+    //tiles: ["https://tiles.arcgis.com/tiles/5T5nSi527N4F7luB/arcgis/rest/services/WHO_Polygon_Basemap_Disputed_Areas_and_Borders_VTP/VectorTileServer/tile/{z}/{y}/{x}.pbf"],
     };
     return style;
 }
@@ -69,11 +72,10 @@ class MapboxMap extends React.Component {
     prepare().then((style) => {
         map = new mapboxgl.Map({
             container: this.mapContainer,
-            // @ts-ignore
-            style: "mapbox://styles/mapbox/dark-v10",//style,
+            style: style, //"mapbox://styles/mapbox/dark-v10"
             center: [this.state.lng, this.state.lat],
             zoom: this.state.zoom,
-            transformRequest: (url, resourceType) => {
+            /*transformRequest: (url : any, resourceType : any) => {
               if (resourceType === "Tile")
               {
                 return {
@@ -84,39 +86,13 @@ class MapboxMap extends React.Component {
                   }
                 }
               }
-              //console.log(url, resourceType)
-             return { url };
-            },
+             return { url }; 
+            }, */
           });
 
         map.on('load', () => {
-          map.addLayer({
-            "id": "road2",
-            "type": "line",
-            "source": {
-                    "type": "vector",
-                    "tiles": ["https://basemaps.arcgis.com/arcgis/rest/services/OpenStreetMap_FTS_v2/VectorTileServer/tile/{z}/{y}/{x}.pbf"]
-            },
-            "source-layer": "road",
-            "layout": {
-                "line-cap": "butt",
-                "line-join": "miter"
-            },
-            "paint": {
-                "line-color": "rgba(65,244,244,0.3)",
-                "line-width": 3
-            }
-        }, 'waterway-label');
+          
         })
-          /*
-        map.on("move", () => {
-        this.setState({
-            lng: map.getCenter().lng.toFixed(4),
-            lat: map.getCenter().lat.toFixed(4),
-            zoom: map.getZoom().toFixed(2),
-        });
-        });
-        */
     })
   }
   
