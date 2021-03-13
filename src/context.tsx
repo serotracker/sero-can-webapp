5767e6507268b0d7917f08a12d2b0f6c3395c975
PRANNOY LAL <prannoylal@gmail.com>
2021-03-13 13:45:58 -0500

    updated react context

--- src/context.tsx
+++ src/context.tsx
@@ -1,8 +1,17 @@
 import React, { createContext, Dispatch, useReducer } from "react";
-import { AggregationFactor, Filters, FilterType, LanguageType, PageState, State } from "./types";
+import {
+  AggregationFactor,
+  Filters,
+  FilterType,
+  LanguageType,
+  PageState,
+  State,
+} from "./types";
 import Translate from "./utils/translate/translateService";
 
-export const AppContext = createContext({} as [State, Dispatch<Record<string, any>>]);
+export const AppContext = createContext(
+  {} as [State, Dispatch<Record<string, any>>]
+);
 
 export function getEmptyFilters(): Filters {
   return {
@@ -16,23 +25,23 @@ export function getEmptyFilters(): Filters {
     isotypes_reported: new Set(),
     specimen_type: new Set(),
     publish_date: new Set([new Date(2019, 1, 1, 1), new Date()]),
-    estimate_grade: new Set()
-  }
+    estimate_grade: new Set(),
+  };
 }
 
 export function getDefaultFilters(): Filters {
   return {
     source_type: new Set([
-      Translate('SourceTypeOptions', ['Preprint']),
-      Translate('SourceTypeOptions', ['Publication']),
-      Translate('SourceTypeOptions', ['InstitutionalReport'])
+      Translate("SourceTypeOptions", ["Preprint"]),
+      Translate("SourceTypeOptions", ["Publication"]),
+      Translate("SourceTypeOptions", ["InstitutionalReport"]),
     ]),
     test_type: new Set(),
     country: new Set(),
     population_group: new Set([
-      Translate('PopulationGroupOptions', ['GeneralPopulation']),
-      Translate('PopulationGroupOptions', ['BloodDonors']),
-      Translate('PopulationGroupOptions', ['ResidualSera']),
+      Translate("PopulationGroupOptions", ["GeneralPopulation"]),
+      Translate("PopulationGroupOptions", ["BloodDonors"]),
+      Translate("PopulationGroupOptions", ["ResidualSera"]),
     ]),
     sex: new Set(),
     age: new Set(),
@@ -41,52 +50,40 @@ export function getDefaultFilters(): Filters {
     specimen_type: new Set(),
     publish_date: new Set([new Date(2019, 1, 1, 1), new Date()]),
     estimate_grade: new Set([
-      Translate('EstimateGradeOptions', ['National']),
-      Translate('EstimateGradeOptions', ['Regional']),
-    ])
-  }
+      Translate("EstimateGradeOptions", ["National"]),
+      Translate("EstimateGradeOptions", ["Regional"]),
+    ]),
+  };
 }
 
 // Note: filters = elements that user has chosen to filter by
 // filter_options = all the elements that users could filter by
 const initialState: State = {
-  healthcheck: '',
   chartAggregationFactor: AggregationFactor.country,
   explore: {
     filters: getEmptyFilters(),
     records: [],
     estimateGradePrevalences: [],
-    metaAnalyzedRecords: [],
-    isLoading: false
+    isLoading: false,
   },
   // TODO: replace this with an obj
   // representing the data page once
   // we put filters there
-  analyze: {
-    filters: getDefaultFilters(),
-    records: [],
-    estimateGradePrevalences: [],
-    metaAnalyzedRecords: [],
-    isLoading: false
-  },
   allFilterOptions: getEmptyFilters(),
   dataPageState: {
     exploreIsOpen: true,
-    showStudiesModal: false,
-    routingOccurred: false
   },
   calendarStartDates: {
     // Important, the fact that we use an hour here tells us that we are using a default value
     minDate: new Date(2019, 1, 1, 1),
-    maxDate: new Date()
+    maxDate: new Date(),
   },
   language: LanguageType.english,
-  updatedAt: '',
+  updatedAt: "",
   showCookieBanner: false,
-  showAnalyzePopup: true,
-  countries : [],
+  countries: [],
   showEstimatePins: true,
-  showCountryHover:true,
+  showCountryHover: true,
 };
 
 const reducer = (state: State, action: Record<string, any>): State => {
@@ -94,37 +91,27 @@ const reducer = (state: State, action: Record<string, any>): State => {
     case "SHOW_COUNTRY_HOVER":
       return {
         ...state,
-        showCountryHover: true
+        showCountryHover: true,
       };
     case "HIDE_COUNTRY_HOVER":
       return {
         ...state,
-        showCountryHover: false
+        showCountryHover: false,
       };
     case "TOGGLE_ESTIMATE_PINS":
       return {
         ...state,
-        showEstimatePins: !state.showEstimatePins
+        showEstimatePins: !state.showEstimatePins,
       };
     case "CLOSE_COOKIE_BANNER":
       return {
         ...state,
-        showCookieBanner: false
+        showCookieBanner: false,
       };
     case "OPEN_COOKIE_BANNER":
       return {
         ...state,
-        showCookieBanner: true
-      };
-    case "OPEN_ANALYZE_POPUP":
-      return {
-        ...state,
-        showAnalyzePopup: true
-      };
-    case "CLOSE_ANALYZE_POPUP":
-      return {
-        ...state,
-        showAnalyzePopup: false
+        showCookieBanner: true,
       };
     case "UPDATE_ESTIMATE_PREVALENCES": {
       const { pageStateEnum, estimateGradePrevalences } = action.payload;
@@ -132,21 +119,21 @@ const reducer = (state: State, action: Record<string, any>): State => {
       const pageState = newState[pageStateEnum as keyof State] as PageState;
       pageState.estimateGradePrevalences = estimateGradePrevalences;
       return newState;
-    };
+    }
     case "UPDATE_COUNTRIES_JSON":
       return {
         ...state,
-        countries: action.payload
+        countries: action.payload,
       };
     case "SELECT_LANGUAGE":
       return {
         ...state,
-        language: action.payload
+        language: action.payload,
       };
     case "GET_ALL_FILTER_OPTIONS":
       return {
         ...state,
-        allFilterOptions: action.payload
+        allFilterOptions: action.payload,
       };
     case "GET_AIRTABLE_RECORDS": {
       const { pageStateEnum, records } = action.payload;
@@ -154,11 +141,11 @@ const reducer = (state: State, action: Record<string, any>): State => {
       const pageState = newState[pageStateEnum as keyof State] as PageState;
       pageState.records = records;
       return newState;
-    };
+    }
     case "UPDATED_AT":
       return {
         ...state,
-        updatedAt: action.payload
+        updatedAt: action.payload,
       };
     case "UPDATE_FILTER": {
       const { pageStateEnum, filterType, filterValue } = action.payload;
@@ -166,22 +153,21 @@ const reducer = (state: State, action: Record<string, any>): State => {
       const pageState = newState[pageStateEnum as keyof State] as PageState;
       pageState.filters[filterType as FilterType] = new Set(filterValue);
       return newState;
-    };
+    }
     // Note: deprecated but leaving here in case we ever need again
     case "UPDATE_META_ANALYSIS": {
-      const { pageStateEnum, metaAnalyzedRecords } = action.payload;
+      const { pageStateEnum } = action.payload;
       const newState = { ...state };
       const pageState = newState[pageStateEnum as keyof State] as PageState;
-      pageState.metaAnalyzedRecords = metaAnalyzedRecords;
       return newState;
-    };
+    }
     case "MAX_MIN_DATES":
       const { minDate, maxDate } = action.payload;
       return {
         ...state,
         calendarStartDates: {
           minDate,
-          maxDate
+          maxDate,
         },
       };
     case "CHANGE_LOADING": {
@@ -190,22 +176,27 @@ const reducer = (state: State, action: Record<string, any>): State => {
       const pageState = newState[pageStateEnum as keyof State] as PageState;
       pageState.isLoading = isLoading;
       return newState;
-    };
+    }
     case "UPDATE_AGGREGATION_FACTOR":
       return { ...state, chartAggregationFactor: action.payload };
     case "UPDATE_EXPLORE_IS_OPEN":
-      return { ...state, dataPageState: { ...state.dataPageState, exploreIsOpen: action.payload } };
+      return {
+        ...state,
+        dataPageState: {
+          ...state.dataPageState,
+          exploreIsOpen: action.payload,
+        },
+      };
     default:
-      return state
-  };
+      return state;
+  }
 };
 
 export const AppContextProvider = (props: Record<string, any>) => {
   const [state, dispatch] = useReducer(reducer, initialState);
 
   return (
-    <AppContext.Provider
-      value={[state, dispatch]}>
+    <AppContext.Provider value={[state, dispatch]}>
       {props.children}
     </AppContext.Provider>
   );
--- src/types.ts
+++ src/types.ts
@@ -1,166 +1,168 @@
-
 export type AirtableRecord = {
-    source_id: string | null,
-    source_name: string | null,
-    lead_organization?: string | null,
-    first_author?: string | null,
-    source_type: string | null,
-    test_type: string | null,
-    specimen_type: string[] | null,
-    isotypes_reported: string[] | null,
-    test_manufacturer?: string[] | null,
-    sensitivity?: number | null,
-    specificity?: number | null,
-    country: string | null,
-    state?: string[] | null,
-    city?: string[] | null,
-    population_group: string | null,
-    sex: string | null,
-    age: string | null,
-    denominator: number | null,
-    seroprevalence: number | null,
-    publish_date?: string[] | string | null,
-    publisher?: string | null,
-    overall_risk_of_bias: string | null,
-    pin_latitude: number,
-    pin_longitude: number,
-    pin_region_type: string | null,
-    study_type?: string | null,
-    sampling_method?: string | null,
-    sampling_start_date?: string | null,
-    sampling_end_date?: string | null,
-    summary?: string | null,
-    url?: string | null,
-    estimate_grade: string | null
+  source_id: string | null;
+  source_name: string | null;
+  lead_org?: string | null;
+  first_author?: string | null;
+  source_type: string | null;
+  test_type: string[] | null;
+  specimen_type: string[] | null;
+  isotypes_reported: string[] | null;
+  manufacturer?: string | null;
+  approving_regulator?: string | null;
+  sensitivity?: number | null;
+  specificity?: number | null;
+  country: string | null;
+  state?: string[] | null;
+  city?: string[] | null;
+  population_group: string | null;
+  sex: string | null;
+  age: string | null;
+  denominator: number | null;
+  seroprevalence: number | null;
+  publish_date?: string[] | string | null;
+  publisher?: string | null;
+  overall_risk_of_bias: string | null;
+  pin_latitude: number;
+  pin_longitude: number;
+  pin_region_type: string | null;
+  study_type?: string | null;
+  sampling_method?: string | null;
+  sampling_start_date?: string | null;
+  sampling_end_date?: string | null;
+  summary?: string | null;
+  url?: string | null;
+  estimate_grade: string | null;
 };
 
 export type AggregatedRecord = {
-    error: number[];
-    n: number;
-    name: string;
-    numStudies: number;
-    seroprevalence: number;
-}
+  error: number[];
+  n: number;
+  name: string;
+  numStudies: number;
+  seroprevalence: number;
+};
 
 // Each filter will be a javascript set
 // TODO: find typing to represent sets
 export type Filters = {
-    source_type: any,
-    test_type: any,
-    country: any,
-    population_group: any,
-    sex: any,
-    age: any,
-    overall_risk_of_bias: any,
-    isotypes_reported: any,
-    specimen_type: any
-    publish_date: any,
-    estimate_grade: any,
+  source_type: any;
+  test_type: any;
+  country: any;
+  population_group: any;
+  sex: any;
+  age: any;
+  overall_risk_of_bias: any;
+  isotypes_reported: any;
+  specimen_type: any;
+  publish_date: any;
+  estimate_grade: any;
 };
 
-export type FilterType = 'country' | 'population_group' | 'sex' | 'age' | 'test_type' | 'source_type' | 'overall_risk_of_bias' | 'isotypes_reported' | 'specimen_type' | 'estimate_grade';
+export type FilterType =
+  | "country"
+  | "population_group"
+  | "sex"
+  | "age"
+  | "test_type"
+  | "source_type"
+  | "overall_risk_of_bias"
+  | "isotypes_reported"
+  | "specimen_type"
+  | "estimate_grade";
 
 export enum LanguageType {
-    french = 'fr',
-    english = 'en'
+  french = "fr",
+  english = "en",
 }
 
 export enum PageStateEnum {
-    // TODO: replace analyze with data here
-    // once our data page uses filters
-    // Analyze can now be deprecated fully bc of Tableau
-    analyze = "analyze",
-    explore = "explore"
+  // TODO: replace analyze with data here
+  // once our data page uses filters
+  explore = "explore",
 }
 
 export type State = {
-    healthcheck: string,
-    chartAggregationFactor: AggregationFactor,
-    explore: PageState,
-    analyze: PageState,
-    allFilterOptions: Filters,
-    updatedAt: string,
-    calendarStartDates: StartDates,
-    dataPageState: DataPageState,
-    language: LanguageType,
-    showCookieBanner: boolean,
-    showAnalyzePopup: boolean,
-    countries: any,
-    showEstimatePins: boolean,
-    showCountryHover: boolean,
+  chartAggregationFactor: AggregationFactor;
+  explore: PageState;
+  allFilterOptions: Filters;
+  updatedAt: string;
+  calendarStartDates: StartDates;
+  dataPageState: DataPageState;
+  language: LanguageType;
+  showCookieBanner: boolean;
+  countries: any;
+  showEstimatePins: boolean;
+  showCountryHover: boolean;
 };
 
 export type StartDates = {
-    minDate: Date,
-    maxDate: Date
-}
+  minDate: Date;
+  maxDate: Date;
+};
 
 export type PageState = {
-    filters: Filters,
-    metaAnalyzedRecords: AggregatedRecord[],
-    records: AirtableRecord[],
-    isLoading: boolean,
-    estimateGradePrevalences: EstimateGradePrevalence[]
-}
+  filters: Filters;
+  records: AirtableRecord[];
+  isLoading: boolean;
+  estimateGradePrevalences: EstimateGradePrevalence[];
+};
 
 export type EstimateGradePrevalence = {
-    testsAdministered: number;
-    geographicalName: string;
-    alpha3Code: string;
-    numberOfStudies: number;
-    localEstimate?: RegionalPrevalenceEstimate,
-    nationalEstimate?: RegionalPrevalenceEstimate,
-    regionalEstimate?: RegionalPrevalenceEstimate,
-    sublocalEstimate?: RegionalPrevalenceEstimate
-}
+  testsAdministered: number;
+  geographicalName: string;
+  alpha3Code: string;
+  numberOfStudies: number;
+  localEstimate?: RegionalPrevalenceEstimate;
+  nationalEstimate?: RegionalPrevalenceEstimate;
+  regionalEstimate?: RegionalPrevalenceEstimate;
+  sublocalEstimate?: RegionalPrevalenceEstimate;
+};
 
 export type RegionalPrevalenceEstimate = {
-    maxEstimate: number;
-    minEstimate: number;
-    numEstimates: number;
-}
+  maxEstimate: number;
+  minEstimate: number;
+  numEstimates: number;
+};
 
 export enum AggregationFactor {
-    country = 'country',
-    population_group = 'population_group',
-    sex = 'sex',
-    age = 'age',
-    test_type = 'test_type',
-    source_type = 'source_type',
-    overall_risk_of_bias = 'overall_risk_of_bias',
-    isotypes_reported = 'isotypes_reported',
+  country = "country",
+  population_group = "population_group",
+  sex = "sex",
+  age = "age",
+  test_type = "test_type",
+  source_type = "source_type",
+  overall_risk_of_bias = "overall_risk_of_bias",
+  isotypes_reported = "isotypes_reported",
 }
 
 export type DataPageState = {
-    exploreIsOpen: boolean,
-    showStudiesModal: false,
-    routingOccurred: boolean
-}
+  exploreIsOpen: boolean;
+};
 
 export type CustomMatcherResult = {
-    pass: boolean
-    message: string
-}
+  pass: boolean;
+  message: string;
+};
 
 export type PostRecordsBody = {
-    filters: {
-        country: String[],
-        source_type: String[],
-        source_name: String[],
-        overall_risk_of_bias: String[],
-        population_group: String[],
-        age: String[],
-        sex: String[],
-        test_type: String[],
-        isotypes_reported: String[],
-        specimen_type: String[],
-        estimate_grade: String[]
-    },
-    sampling_start_date: Date | null,
-    sampling_end_date: Date | null,
-    sorting_key: String,
-    reverse: Boolean,
-    per_page: Number,
-    page_index: Number,
-    columns: String[]
-}
+  filters: {
+    country: String[];
+    source_type: String[];
+    source_name: String[];
+    overall_risk_of_bias: String[];
+    population_group: String[];
+    age: String[];
+    sex: String[];
+    test_type: String[];
+    isotypes_reported: String[];
+    specimen_type: String[];
+    estimate_grade: String[];
+  };
+  sampling_start_date: Date | null;
+  sampling_end_date: Date | null;
+  sorting_key: String;
+  reverse: Boolean;
+  per_page: Number;
+  page_index: Number;
+  columns: String[];
+};
