5fa6480a74161590f28cdfec6a1cc2acb9d3aa4d
Austin Atmaja <austin.atmaja@gmail.com>
2021-04-01 22:43:43 -0400

    Styling, putting strings into translations files

--- src/components/shared/Footer.tsx
+++ src/components/shared/Footer.tsx
@@ -16,17 +16,30 @@ export const Footer = () => {
     return 'regular col-auto h-100 flex center-item';
   }
 
-  const isMobileDeviceOrTablet = useMediaQuery({ maxDeviceWidth: mobileDeviceOrTabletWidth })
+  const isMobileDeviceOrTablet = useMediaQuery({ maxDeviceWidth: mobileDeviceOrTabletWidth });
+
+  const lancetId = "https://www.thelancet.com/journals/laninf/article/PIIS1473-3099(20)30631-9/fulltext#%20"
 
   return (
-    <header className="App-footer col-12 px-sm-2">
-        <div className="App-footer-row">
-            <div>{`${Translate("DatabaseUptoDateTo")}: ${updatedAt}`}</div>
+    <header className={isMobileDeviceOrTablet ? "App-footer-mob col-12 px-2" : "App-footer col-12 px-2"}>
+        <div className={isMobileDeviceOrTablet ? "App-footer-item middle-item px-2 py-1" : "App-footer-item left-item px-2"}>
+            <div>
+              <span>{Translate("Footer", ["LastUpdated"])}: </span>
+              <span className="updated-date-string">{updatedAt}</span>
+            </div>
         </div>
-        <div className="App-footer-row">
-            <Link className="p-1 footer-link" to={withLocaleUrl("PrivacyPolicy")}>{Translate('PrivacyPolicy')}</Link> |
-            <Link className="p-1 footer-link" to={withLocaleUrl("CookiePolicy")}>{Translate('CookiePolicy')}</Link> |
+        <div className={isMobileDeviceOrTablet ? "App-footer-item middle-item px-2 py-1" : "App-footer-item left-item px-2"}>
+            <Link className="p-1 footer-link" to={withLocaleUrl("PrivacyPolicy")}>{Translate('PrivacyPolicy')}</Link>  |
+            <Link className="p-1 footer-link" to={withLocaleUrl("CookiePolicy")}>{Translate('CookiePolicy')}</Link>  |
             <Link className="p-1 footer-link" to={withLocaleUrl("TermsOfUse")}>{Translate('TermsOfUse')}</Link>
+        </div>
+        <div className={isMobileDeviceOrTablet ? "App-footer-item middle-item px-2 py-1" : "App-footer-item right-item px-2"}>
+            <div>
+              <span>{Translate("Footer", ["CiteAs"])}</span> 
+              <a href={lancetId} target="__blank" rel="noopener noreferrer" className="cite-link">
+                <span className="italics">{Translate("Footer", ["LancetInfDis"])}</span> {Translate("Footer", ["Article"])}
+              </a> 
+            </div>
         </div>
     </header>
   )
--- src/components/sidebar/right-sidebar/RightSidebar.tsx
+++ src/components/sidebar/right-sidebar/RightSidebar.tsx
@@ -10,7 +10,7 @@ interface SideBarProps {
 
 export default function RightSidebar({page}: SideBarProps) {
   return (
-    <div className="justify-content-between fill sidebar-container">
+    <div className="justify-content-between fill sidebar-container right-sidebar">
       <div className="filters-container mb-3">
         <Filters page={page}/>
         <Datepicker page={page}/>
