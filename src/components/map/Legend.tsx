1e0415d0de79336cdb110fafe4c759f19d96c106
Brett <brettdziedzic@gmail.com>
2021-04-03 19:54:50 -0600

    additonal legend changes

--- src/components/map/Legend.tsx
+++ src/components/map/Legend.tsx
@@ -8,7 +8,6 @@ import { MapSymbology } from "components/map/MapConfig"
 
 export default function Legend() {
   const [state, dispatch] = useContext(AppContext);
-  const isMobileDeviceOrTablet = useMediaQuery({ maxWidth: mobileDeviceOrTabletWidth })
   const handleLegendToggle = (e: React.MouseEvent<HTMLElement>) => {
     const layerName: string = e.currentTarget.id;
     switch (layerName) {
@@ -25,28 +24,28 @@ export default function Legend() {
   };
 
   return (
-    <div className={isMobileDeviceOrTablet ? "info flex legend-mobile center-item" : "info flex legend center-item"}>
+    <div className="info flex legend center-item">
       <div className="flex legend-container" key={Math.random()}>
-        <div className="legend-item" id="National" onClick={handleLegendToggle}>
+        <div className="legend-item mb-2" id="National" onClick={handleLegendToggle}>
           <i className="circleBase" style={{ background: MapSymbology.StudyFeature.National.Color }}></i>
           <p>{Translate("NationalStudies")}</p>
-          <input className="checkbox" type="checkbox" checked={state.explore.legendLayers.National} readOnly/>
+          <input className="ui checkbox" type="checkbox" checked={state.explore.legendLayers.National} readOnly/>
         </div>
-        <div className="legend-item" id="Regional" onClick={handleLegendToggle}>
+        <div className="legend-item mb-2" id="Regional" onClick={handleLegendToggle}>
           <i className="circleBase" style={{ background: MapSymbology.StudyFeature.Regional.Color }}></i>
           <p>{Translate("RegionalStudies")}</p>
-          <input className="checkbox" type="checkbox" checked={state.explore.legendLayers.Regional} readOnly/>
+          <input className="ui checkbox" type="checkbox" checked={state.explore.legendLayers.Regional} readOnly/>
         </div>
-        <div className="legend-item" id="Local" onClick={handleLegendToggle}>
+        <div className="legend-item mb-2" id="Local" onClick={handleLegendToggle}>
           <i className="circleBase" style={{ background: MapSymbology.StudyFeature.Local.Color }}></i>
           <p>{Translate("LocalStudies")}</p>
-          <input className="checkbox" type="checkbox" checked={state.explore.legendLayers.Local} readOnly/>
+          <input className="ui checkbox" type="checkbox" checked={state.explore.legendLayers.Local} readOnly/>
         </div>
-        <div className="legend-item">
+        <div className="legend-item mb-2">
           <i className="block" style={{ background: MapSymbology.CountryFeature.HasData.Color, outlineWidth: 1, outlineStyle: "solid" }}></i>
           <p>{Translate("CountryEstimatesExist")}</p>
         </div>
-        <div className="legend-item">
+        <div className="legend-item mb-2">
           <i className="block" style={{ background: MapSymbology.CountryFeature.Default.Color, outlineWidth: 1, outlineStyle: "solid" }}></i>
           <p>{Translate("CountryEstimatesNotExist")}</p>
         </div>
