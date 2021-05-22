b6a1833bf23bbbf8866bf426a09e2a543b79ab86
Brett <brettdziedzic@gmail.com>
2021-05-22 15:47:21 -0600

    Added partnership dropdown

--- src/components/pages/Studies/Studies.tsx
+++ src/components/pages/Studies/Studies.tsx
@@ -11,41 +11,40 @@ import PartnershipsConfig from "PartnershipsConfig";
 export default function Studies() {
   const isMobileDeviceOrTablet = useMediaQuery({ maxDeviceWidth: mobileDeviceOrTabletWidth });
   const [state, dispatch] = useContext(AppContext);
-  const { url, path } = useRouteMatch();
-  const { name } = useParams<{name: string}>();
-  const config = PartnershipsConfig.Country.find(x => x.routeName === name)
+  const { name } = useParams<{ name: string }>();
+  const config = PartnershipsConfig.Country.find((x) => x.routeName === name);
 
   return (
     <>
       {config !== undefined ? (
         <>
-        <div className="mx-auto col-9">
-          <div style={{ height: "700px" }}>
-            <div className="mt-4">
-              <h1>Study Name Here</h1>
+          <div className="mx-auto col-9">
+            <div style={{ height: "700px" }}>
+              <div className="mt-4">
+                <h1>Study Name Here</h1>
+              </div>
+              <div className="col-12 px-0 h-100 flex">
+                <Map mapConfig={config.mapboxMapOptions} />
+              </div>
             </div>
-            <div className="col-12 px-0 h-100 flex">
-              <Map mapConfig={config.mapboxMapOptions} />
-            </div>
+            <TableauEmbed
+              url={config.tableauUrl}
+              key={config.tableauUrl}
+              desktopOptions={{
+                width: "80vw",
+                height: "4100px",
+              }}
+              mobileOptions={{
+                width: "90vw",
+                height: "2800px",
+              }}
+            />
           </div>
-          <TableauEmbed
-            url={config.tableauUrl}
-            key={config.tableauUrl}
-            desktopOptions={{
-              width: "80vw",
-              height: "4100px",
-            }}
-            mobileOptions={{
-              width: "90vw",
-              height: "2800px",
-            }}
-          />
-        </div>
-        <MaintenanceModal isOpen={isMaintenanceMode} headerText={""} />
+          <MaintenanceModal isOpen={isMaintenanceMode} headerText={""} />
         </>
-      ) : 
-      <Redirect to="/ " />
-      }
+      ) : (
+        <Redirect to="/ " />
+      )}
     </>
   );
 }
