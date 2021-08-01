5b529d46d9d4877fdebda61a7145af90b3b99972
Brett <brettdziedzic@gmail.com>
2021-07-31 19:46:35 -0600

    mobile nav menu

--- src/components/shared/NavBar.tsx
+++ src/components/shared/NavBar.tsx
@@ -1,5 +1,3 @@
-import { faFileAlt, faGlobeAmericas, faUsers, faTable, faChartBar } from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React, { useContext, useEffect, useState } from 'react'
 import { useMediaQuery } from 'react-responsive'
 import { Link, useHistory, useLocation } from 'react-router-dom'
@@ -10,14 +8,14 @@ import { LanguageType } from '../../types'
 import Translate from '../../utils/translate/translateService'
 import { withLocaleUrl } from '../../utils/utils'
 import PartnershipsDropDown from './PartnershipsDropDown'
+import { Dropdown } from "semantic-ui-react";
 
 export const NavBar = () => {
   const [tab, setTab] = useState("");
   const [state, dispatch] = useContext(AppContext);
 
   const getTabClass = (tabName: string) => {
-    const style = tabName === tab ? 'bold col-auto h-100 flex center-item' : 'regular col-auto h-100 flex center-item';
-    return isMobileDeviceOrTablet ? style.concat(" px-1") : style;
+    return tab.includes(tabName) ? 'nav__item--active col-auto h-100 flex center-item' : 'nav__item col-auto h-100 flex center-item';
   }
 
   const usePageViews = () => {
@@ -53,56 +51,85 @@ export const NavBar = () => {
       <div className="App-title col-auto py-3 px-0 flex left">
         <Link to={withLocaleUrl("Explore")} className="flex">
           <img src={MultiColorIcon} width={23} height={23} alt="" />
-          {!isMobileDeviceOrTablet && (<div className="col-auto px-2" >SeroTracker</div>)}
+          <div className="col-auto px-2" >SeroTracker</div>
         </Link>
       </div>
-      <div className={`App-tabs col-auto space-evenly p-0 ${isMobileDeviceOrTablet ? 'fill' : ''}`}>
-        <div className={getTabClass('/Explore')}>
-          <Link to={withLocaleUrl("Explore")}>
-            {isMobileDeviceOrTablet ?
-              <FontAwesomeIcon icon={faGlobeAmericas} size="lg" /> :
-              Translate('Explore')}
-          </Link>
-        </div>
-        <div className={getTabClass('/Analyze')}>
-          <Link to={withLocaleUrl("Analyze")}>
-            {isMobileDeviceOrTablet ?
-              <FontAwesomeIcon icon={faChartBar} size="lg" /> :
-              Translate('Analyze')}
-          </Link>
-        </div>
-        <div className={getTabClass('/Data')}>
-          <Link to={withLocaleUrl("Data")}>
-            {isMobileDeviceOrTablet ?
-              <FontAwesomeIcon icon={faTable} size="lg" /> :
-              Translate('Data')}
-          </Link>
-        </div>
-        <div className={getTabClass('/Publications')}>
-          <Link to={withLocaleUrl("Publications")}>
-            {isMobileDeviceOrTablet ?
-              <FontAwesomeIcon icon={faFileAlt} size="lg" /> :
-              Translate('Publications')}
-          </Link>
-        </div>
-        <div className={getTabClass('/About')}>
-          <Link to={withLocaleUrl("About")}>
-            {isMobileDeviceOrTablet ?
-              <FontAwesomeIcon icon={faUsers} size="lg" /> :
-              Translate('About')}
-          </Link>
-        </div>
-        <div className={getTabClass('/Partnerships')}>
-          <PartnershipsDropDown />
-        </div>
-        <div className={getTabClass('/Language') + " cursor"} onClick={() => changeLanguages()}>
-          <div>
-            {state.language === LanguageType.english ?
-              (isMobileDeviceOrTablet ? "Fr" : "Français") :
-              (isMobileDeviceOrTablet ? "En" : "English")}
-          </div>
-        </div>
-      </div>
+      { isMobileDeviceOrTablet ? mobileNav() : desktopNav(isMobileDeviceOrTablet,getTabClass, changeLanguages, state.language) }
     </header >
   )
+}
+
+const mobileNav = () => {
+  return (<Dropdown icon='bars' direction='left'>
+  <Dropdown.Menu>
+    <Dropdown.Item>
+      <Link to={withLocaleUrl("Explore")}>
+          {Translate('Explore')}
+      </Link>
+    </Dropdown.Item>
+    <Dropdown.Item>
+      <Link to={withLocaleUrl("Analyze")}>
+        {Translate('Analyze')}
+      </Link>
+    </Dropdown.Item>
+    <Dropdown.Item>
+      <Link to={withLocaleUrl("Data")}>
+        {Translate('Data')}
+      </Link>
+    </Dropdown.Item>
+    <Dropdown.Item>
+      <Link to={withLocaleUrl("Publications")}>
+        {Translate('Publications')}
+      </Link>
+    </Dropdown.Item>
+    <Dropdown.Item>
+      <Link to={withLocaleUrl("About")}>
+        {Translate('About')}
+      </Link>
+    </Dropdown.Item>
+    <Dropdown.Item>
+      <PartnershipsDropDown />
+    </Dropdown.Item>
+  </Dropdown.Menu>
+</Dropdown>)
+}
+
+const desktopNav = (isMobileDeviceOrTablet: boolean, getTabClass: any, changeLanguages:any, language: any) => {
+  return (<div className={`App-tabs col-auto space-evenly p-0 ${isMobileDeviceOrTablet ? 'fill' : ''}`}>
+  <div className={getTabClass('/Explore')}>
+    <Link to={withLocaleUrl("Explore")}>
+      {Translate('Explore')}
+    </Link>
+  </div>
+  <div className={getTabClass('/Analyze')}>
+    <Link to={withLocaleUrl("Analyze")}>
+      {Translate('Analyze')}
+    </Link>
+  </div>
+  <div className={getTabClass('/Data')}>
+    <Link to={withLocaleUrl("Data")}>
+      {Translate('Data')}
+    </Link>
+  </div>
+  <div className={getTabClass('/Publications')}>
+    <Link to={withLocaleUrl("Publications")}>
+      {Translate('Publications')}
+    </Link>
+  </div>
+  <div className={getTabClass('/About')}>
+    <Link to={withLocaleUrl("About")}>
+      {Translate('About')}
+    </Link>
+  </div>
+  <div className={getTabClass('/Partnerships')}>
+    <PartnershipsDropDown />
+  </div>
+  <div className={getTabClass('/Language') + " cursor"} onClick={() => changeLanguages()}>
+    <div>
+      {language === LanguageType.english ?
+        (isMobileDeviceOrTablet ? "Fr" : "Français") :
+        (isMobileDeviceOrTablet ? "En" : "English")}
+    </div>
+  </div>
+</div>)
 }
