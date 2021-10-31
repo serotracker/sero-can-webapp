dfac0f42687878a4c67b8fcd664b0c8686ad782e
Brett <brettdziedzic@gmail.com>
2021-10-31 14:33:34 -0600

    added partnership render switch

--- src/components/pages/Partnerships/Partnerships.tsx
+++ src/components/pages/Partnerships/Partnerships.tsx
@@ -1,9 +1,26 @@
 import React from "react";
 import { Redirect, useParams } from "react-router-dom";
 import { isMaintenanceMode } from "../../../constants";
+import { Partnership } from "types";
 import MaintenanceModal from "../../shared/MaintenanceModal";
 import PartnershipsConfig from "PartnershipsConfig";
 import CanadaPartnership from 'components/pages/Partnerships/CanadaPartnership'
+
+type PartnershipProps = {
+  partnershipConfig: Partnership;
+};
+
+function RenderSelectedPartnershipComponent({partnershipConfig}: PartnershipProps){
+  let partnership = undefined
+  switch(partnershipConfig.routeName) {
+    case 'Canada':
+      partnership = <CanadaPartnership partnershipConfig={partnershipConfig}/>
+      break;
+    // Add more cases for new parnership components here.
+  } 
+
+  return partnership
+}
 
 export default function Partnerships() {
   const {name} = useParams<{ name: string }>();
@@ -15,7 +32,7 @@ export default function Partnerships() {
         <>
           <div className="col-12 page pb-6">
             <div className="col-10">
-              <CanadaPartnership partnershipConfig={config}/>
+              {RenderSelectedPartnershipComponent({partnershipConfig: config})}
             </div>
           </div>
           <MaintenanceModal isOpen={isMaintenanceMode} headerText={""} />
