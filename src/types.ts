655005acabbdcb76d0d3f3cbd2e87af1c7f1e062
prannoy lal <prannoylal@gmail.com>
2021-11-06 09:44:30 -0400

    setup filter

--- src/types.ts
+++ src/types.ts
@@ -52,6 +52,7 @@ export type FiltersConfig = {
   overall_risk_of_bias: string[];
   isotypes_reported: string[];
   specimen_type: string[];
+  antibody_target: string[],
   publish_date: Date[];
   estimate_grade: string[];
   unity_aligned_only: boolean;
@@ -71,6 +72,7 @@ export type FilterType =
   | "overall_risk_of_bias"
   | "isotypes_reported"
   | "specimen_type"
+  | "antibody_target"
   | "estimate_grade"
   | "unity_aligned_only";
 
