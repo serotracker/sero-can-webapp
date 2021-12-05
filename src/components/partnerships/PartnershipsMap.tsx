8b0a4267f720faf0be2ea2d8a00870eb037db000
Austin Atmaja <austin.atmaja@gmail.com>
2021-12-05 12:05:29 -0500

    Fix CITF map positioning and add loading state

--- src/components/partnerships/PartnershipsMap.tsx
+++ src/components/partnerships/PartnershipsMap.tsx
@@ -13,14 +13,70 @@ type PartnerShipsMapProps = {
   partnershipconfig?: Partnership;
 };
 
-const desktopMap = (
-  partnershipconfig: Partnership,
-  estimateGradePrevalence: EstimateGradePrevalence | undefined,
-  records: AirtableRecord[]
-) => {
-  return (
-    <>
-      <div className="col-9 h-100">
+export default function PartnerShipsMap({ partnershipconfig }: PartnerShipsMapProps) {
+  const [state] = useContext(AppContext);
+  const [records, setRecords] = useState<AirtableRecord[]>([]);
+  const [estimateGradePrevalence, setEstimateGradePrevalence] = useState<EstimateGradePrevalence | undefined>();
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  useEffect(() => {
+    const api = new httpClient();
+    const getPartnershipsData = async () => {
+      setIsLoading(true);
+      const { records, estimateGradePrevalences } = await api.getCountryPartnershipData({country: [partnershipconfig?.routeName]});
+      setRecords(records);
+      setEstimateGradePrevalence(estimateGradePrevalences[0]);
+      setIsLoading(false);
+    }
+    getPartnershipsData();
+  }, [partnershipconfig, state.explore.estimateGradePrevalences])
+  const isMobileDeviceOrTablet = useMediaQuery({ maxDeviceWidth: mobileDeviceOrTabletWidth })
+
+  if (partnershipconfig === undefined) {
+    return <></>;
+  }
+
+  const desktopMap = (
+    partnershipconfig: Partnership,
+    estimateGradePrevalence: EstimateGradePrevalence | undefined,
+    records: AirtableRecord[]
+  ) => {
+    return (
+      <>
+        <div className="col-9 h-100">
+          <Loader indeterminate active={isLoading}/>
+          <MapboxMap
+            mapConfig={partnershipconfig.mapboxMapOptions}
+            countriesConfig={{
+              estimateGradePrevalences: estimateGradePrevalence ? [estimateGradePrevalence] : undefined,
+              countryFocus: partnershipconfig?.iso3,
+            }}
+            studyPinsConfig={{
+              records: records,
+            }}
+          />
+        </div>
+        <div className="col-3 h-100">
+          <Legend hideLayers />
+            <div className="mt-1">{Translate("PartnershipsView", ["MapNote1"])}</div>
+            <div className="mt-1">
+                {Translate("PartnershipsView", ["MapNote2"], {
+                    WEBSITE: "ourworldindata.org",
+                })}
+            </div>
+        </div>
+      </>
+    );
+  };
+  
+  // Note: Loading spinner doesn't work very well on mobile
+  // Not a priority to implement because mobile is deprecated anyways
+  const mobileMap = (
+    partnershipconfig: Partnership,
+    estimateGradePrevalence: EstimateGradePrevalence | undefined,
+    records: AirtableRecord[]
+  ) => {
+    return (
         <MapboxMap
           mapConfig={partnershipconfig.mapboxMapOptions}
           countriesConfig={{
@@ -31,64 +87,13 @@ const desktopMap = (
             records: records,
           }}
         />
-      </div>
-      <div className="col-3 h-100">
-        <Legend hideLayers />
-          <div className="mt-1">{Translate("PartnershipsView", ["MapNote1"])}</div>
-          <div className="mt-1">
-              {Translate("PartnershipsView", ["MapNote2"], {
-                  WEBSITE: "ourworldindata.org",
-              })}
-          </div>
-      </div>
-    </>
-  );
-};
-
-const mobileMap = (
-  partnershipconfig: Partnership,
-  estimateGradePrevalence: EstimateGradePrevalence | undefined,
-  records: AirtableRecord[]
-) => {
-  return (
-      <MapboxMap
-        mapConfig={partnershipconfig.mapboxMapOptions}
-        countriesConfig={{
-          estimateGradePrevalences: estimateGradePrevalence ? [estimateGradePrevalence] : undefined,
-          countryFocus: partnershipconfig?.iso3,
-        }}
-        studyPinsConfig={{
-          records: records,
-        }}
-      />
-  );
-};
-
-export default function PartnerShipsMap({ partnershipconfig }: PartnerShipsMapProps) {
-  const [state] = useContext(AppContext);
-  const [records, setRecords] = useState<AirtableRecord[]>([]);
-  const [estimateGradePrevalence, setEstimateGradePrevalence] = useState<EstimateGradePrevalence | undefined>();
-
-  useEffect(() => {
-    const api = new httpClient();
-    const getPartnershipsData = async () => {
-      const { records, estimateGradePrevalences } = await api.getCountryPartnershipData({country: [partnershipconfig?.routeName]});
-      setRecords(records);
-      setEstimateGradePrevalence(estimateGradePrevalences[0]);
-    }
-    getPartnershipsData()
-  }, [partnershipconfig, state.explore.estimateGradePrevalences])
-  const isMobileDeviceOrTablet = useMediaQuery({ maxDeviceWidth: mobileDeviceOrTabletWidth })
-
-  if (partnershipconfig === undefined) {
-    return <></>;
-  }
+    );
+  };
 
   const Map = isMobileDeviceOrTablet ? mobileMap(partnershipconfig, estimateGradePrevalence, records): desktopMap(partnershipconfig, estimateGradePrevalence, records)
   const mapHeight = isMobileDeviceOrTablet ? "50vw" : "70vh"
   return (
     <div style={{ height: mapHeight }} className="row">
-    <Loader indeterminate active={state.explore.isLoading} />
       {Map}
     </div>
   );
