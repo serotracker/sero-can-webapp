9e9b77169b99b86df4191a7313c22fd8094ea8ba
prannoy lal <prannoylal@gmail.com>
2021-12-05 21:30:26 -0500

    setup new dateslider + styling

--- src/components/sidebar/right-sidebar/Filters.tsx
+++ src/components/sidebar/right-sidebar/Filters.tsx
@@ -9,6 +9,7 @@ import Translate, { getCountryName } from "../../../utils/translate/translateSer
 import InformationIcon from "../../shared/InformationIcon";
 import SectionHeader from "./SectionHeader";
 import Datepicker from "./datepicker/Datepicker";
+import NewDatepicker from './new-datepicker/NewDatepicker';
 import "./Filters.css";
 import { LanguageType } from "../../../types";
 
@@ -210,6 +211,7 @@ export default function Filters({ page }: FilterProps) {
         </div>
       </div>
       <Datepicker page={page}/>
+      <NewDatepicker/>
     </div>
   )
 }
