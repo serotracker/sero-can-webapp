6772c8856d29cfbfc0948fad09c2c8a873594ca5
Flora Guo <70130527+floraguolr@users.noreply.github.com>
2022-01-31 20:26:49 -0800

    updated analyze (en, fr) and canada (en) links (#337)
    * updated analyze (en, fr) and canada (en) links
    
    * added canada (fr) link
    
    * update canada tableau (fr) link
    
    * Adjust height
    
    Co-authored-by: Austin Atmaja <austin.atmaja@gmail.com>

--- src/constants.ts
+++ src/constants.ts
@@ -8,12 +8,12 @@ export const mapZIndex = {
 }
 
 export const ANALYZE_URLS = {
-    "en": "https://public.tableau.com/views/SeroTrackerAnalyse/GlobalAnalyse?:language=en&:display_count=y&publish=yes&:origin=viz_share_link",
+    "en": "https://public.tableau.com/views/SeroTrackerAnalyzeEN/GlobalAnalyze?:language=en-US&:display_count=n&:origin=viz_share_link",
     "fr": "https://public.tableau.com/views/SeroTrackerAnalyseFR/GlobalAnalyse?:language=en-US&publish=yes&:display_count=n&:origin=viz_share_link"
 }
 export const CANADA_URLS = {
-    "en": "https://public.tableau.com/views/CanadaianDetail/Canadiandetail?:language=en-US&publish=yes&:display_count=n&:origin=viz_share_link",
-    "fr": "https://public.tableau.com/views/Canada-FR/CanadianchartsFR?:language=en-US&publish=yes&:display_count=n&:origin=viz_share_link"
+    "en": "https://public.tableau.com/views/CanadaEN/Canadiancharts?:language=en-US&publish=yes&:display_count=n&:origin=viz_share_link",
+    "fr": "https://public.tableau.com/views/CanadaFR/Canadiancharts?:language=en-US&:display_count=n&:origin=viz_share_link"
 }
 
 
