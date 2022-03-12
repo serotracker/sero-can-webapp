c902fa9cc6275fb9a04d5c818ce699eb21a9bfd0
HimanshuRanka <himanshuranka2612@gmail.com>
2022-03-12 17:42:26 -0500

    changed popup classname to avoid overlap

--- src/components/shared/NewsletterPopup.tsx
+++ src/components/shared/NewsletterPopup.tsx
@@ -20,14 +20,14 @@ export default function NewsletterPopup() {
     }
 
     return (
-        <div className={"popup"} style={{visibility: hidden ? "hidden" : "visible"}}>
-            <Button className={"popup-close-button"} icon onClick={onClickHandle}>
-                <Icon className={"popup-close-button-icon"} name={"close"} />
+        <div className={"newsletter-popup"} style={{visibility: hidden ? "hidden" : "visible"}}>
+            <Button className={"newsletter-popup-close-button"} icon onClick={onClickHandle}>
+                <Icon className={"newsletter-popup-close-button-icon"} name={"close"} />
             </Button>
             <iframe
                 src="https://serotracker.substack.com/embed"
                 width="480" height="320"
-                className={"popup-content"}
+                className={"newsletter-popup-content"}
                 frameBorder="0"
                 scrolling="no"
             />
