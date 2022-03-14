b3a67acb471ad582a1602198d507ce567b7e781b
simonarocco <#19992022Engineer>
2022-03-14 15:28:20 -0400

    Updates

---
+++ precache-manifest.9030dec9cecd25a9bf3c46abe9409ec7.js
@@ -0,0 +1,166 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0c489ad86f0c4c00dc04c03ef5b8fd1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4b05c0b795d7caf64f15",
+    "url": "/static/css/2.988a3396.chunk.css"
+  },
+  {
+    "revision": "7077d2d79054d0104e1c",
+    "url": "/static/css/main.74dfa1e4.chunk.css"
+  },
+  {
+    "revision": "4b05c0b795d7caf64f15",
+    "url": "/static/js/2.ca7e2f55.chunk.js"
+  },
+  {
+    "revision": "99b61851bd3233a2f828464caf365472",
+    "url": "/static/js/2.ca7e2f55.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7077d2d79054d0104e1c",
+    "url": "/static/js/main.b952476b.chunk.js"
+  },
+  {
+    "revision": "518dd9adf09ae9d07bfcb0787e858a63",
+    "url": "/static/js/mapbox-gl-csp-worker.fbc30435.worker.js"
+  },
+  {
+    "revision": "e5fc4ea767c79f190582",
+    "url": "/static/js/runtime-main.9d5e18d9.js"
+  },
+  {
+    "revision": "d48720c36674aa91383acef9b26ca5e3",
+    "url": "/static/media/1200px-The_Economist_Logo.svg.d48720c3.png"
+  },
+  {
+    "revision": "73b9602cd0e2cb63c2818531565c2530",
+    "url": "/static/media/3622.1582633003.73b9602c.png"
+  },
+  {
+    "revision": "3ee92577baa18e326a6861184d03eefc",
+    "url": "/static/media/Bloomberg_Logo.3ee92577.jpg"
+  },
+  {
+    "revision": "a9aeed43c224ad35cd21b563f0987f70",
+    "url": "/static/media/BrokenAb.a9aeed43.gif"
+  },
+  {
+    "revision": "4149acbd2e0336a52ef9e4b188277fe1",
+    "url": "/static/media/CITF-logo-BIL-700.4149acbd.png"
+  },
+  {
+    "revision": "c6a3b4065ec4872761bf026e27ca1dd3",
+    "url": "/static/media/Forbes-logo.c6a3b406.png"
+  },
+  {
+    "revision": "fdc4df403932b56f6368fa92d04a68ba",
+    "url": "/static/media/HIMSSlogo_Hfullcolor_RGB.fdc4df40.png"
+  },
+  {
+    "revision": "a93736ac68df553221cc2bf85634aa70",
+    "url": "/static/media/IndustryUpdatePre_2020.a93736ac.png"
+  },
+  {
+    "revision": "14c5681c16eea076893907b1b935e584",
+    "url": "/static/media/InsuringTheEconomy.14c5681c.png"
+  },
+  {
+    "revision": "58cfe948a0cac24aa0d4e8d371d5e83a",
+    "url": "/static/media/JournalQuebec_Logo2013-.58cfe948.png"
+  },
+  {
+    "revision": "9c54a7db23a98cb6500cf9a3d4cb1334",
+    "url": "/static/media/LancetIDArticle.9c54a7db.png"
+  },
+  {
+    "revision": "48a0e04504cb26209dc7f2fee46de382",
+    "url": "/static/media/New-York-Times-Logo8x6_0.48a0e045.png"
+  },
+  {
+    "revision": "69693f7a067a707f6e41f8363c6d827f",
+    "url": "/static/media/PlosOnePub.69693f7a.png"
+  },
+  {
+    "revision": "0b43b193f3eb1f3db6868b3286234b31",
+    "url": "/static/media/Prepublication.0b43b193.png"
+  },
+  {
+    "revision": "669b184d041c2dc98dea97cce88cce9b",
+    "url": "/static/media/SeroTrackerBloodBank_July_2020.669b184d.png"
+  },
+  {
+    "revision": "157d7250792423f69b75ff3d1aaef53d",
+    "url": "/static/media/SerotrackerLogo.157d7250.svg"
+  },
+  {
+    "revision": "1fe9c1caac71c33ba2c5d84ea5b33fb9",
+    "url": "/static/media/UCalgary.1fe9c1ca.svg"
+  },
+  {
+    "revision": "ce61287b17ffd5a38b70836a828577d0",
+    "url": "/static/media/University-Of-Calgary-Logo.ce61287b.png"
+  },
+  {
+    "revision": "92cca4d5f0a395e9d92cd66a1641cb8f",
+    "url": "/static/media/WHO-EN-C-H.92cca4d5.png"
+  },
+  {
+    "revision": "e509206a2c474929864c949de2996434",
+    "url": "/static/media/WHO-FR-C-H.e509206a.png"
+  },
+  {
+    "revision": "fd7fe07a4bf0eb75b5306db21840ab84",
+    "url": "/static/media/YearInReview2020.fd7fe07a.png"
+  },
+  {
+    "revision": "72f203723d047233b1b3dcb50fe446b9",
+    "url": "/static/media/airtable-icon.72f20372.svg"
+  },
+  {
+    "revision": "8628d1b7399362be554c14f175fc50bd",
+    "url": "/static/media/chi_logo_89px.8628d1b7.png"
+  },
+  {
+    "revision": "62e4fbc9aa3a2e1fd9038a14d776f56a",
+    "url": "/static/media/cision_logo.62e4fbc9.svg"
+  },
+  {
+    "revision": "40d974350e82e633262603de5d5a5fb0",
+    "url": "/static/media/covidend-global-logo.40d97435.png"
+  },
+  {
+    "revision": "18ac378a2243fe901251d85b2ce3ba1c",
+    "url": "/static/media/joule.18ac378a.png"
+  },
+  {
+    "revision": "18b7513fd6750368e28bbb9da834d221",
+    "url": "/static/media/marcia news.18b7513f.png"
+  },
+  {
+    "revision": "f902dbfa854e1652bd5f4a0c7e6645f9",
+    "url": "/static/media/mars.f902dbfa.jpg"
+  },
+  {
+    "revision": "46394929b944731356382b0d1055797c",
+    "url": "/static/media/mcgill-university-logo-png-transparent-cropped.46394929.png"
+  },
+  {
+    "revision": "f6f0eba5f94bba930139d3d8515262fd",
+    "url": "/static/media/mckinsey_logo.f6f0eba5.png"
+  },
+  {
+    "revision": "18734b66d431b25b4882d1eb300ad255",
+    "url": "/static/media/oxford.18734b66.jpg"
+  },
+  {
+    "revision": "a1b9e73c93a0e2e185324b462a129687",
+    "url": "/static/media/public-health-agency.a1b9e73c.svg"
+  },
+  {
+    "revision": "31e73eb2dc09c1a42ad8b5da826cb863",
+    "url": "/static/media/waterloo.31e73eb2.png"
+  }
+]);
