9da81f7fdd861fd99eb565d7c5f752e93c3200c7
Flora Guo <70130527+floraguolr@users.noreply.github.com>
2022-03-21 19:13:30 -0700

    Revert "fix data page download / github button spacing"
    This reverts commit 83185f476379abbedfbec59ba611a8f706e06198.

--- src/components/sidebar/left-sidebar/LeftSidebar.tsx
+++ src/components/sidebar/left-sidebar/LeftSidebar.tsx
@@ -62,7 +62,7 @@ export default function LeftSidebar({ page }: SideBarProps) {
       </span>
       </div>
 
-      <div className="d-flex justify-content-center mt-1">
+      <div className="d-flex justify-content-center mt-1 full-width-button">
       <span style={{margin: "0"}} id={PAGE_HASHES.Explore.AccessGithub}>
         <DownloadButton
         {...githubDownloadProps}
