8f7f6e00cedd652e6b4de2e9fe72a39e27f9ab64
HimanshuRanka <himanshuranka2612@gmail.com>
2022-03-29 21:00:33 +0000

    Deploying to gh-pages from  @ 7d7673013a3440a2d214ca155d2dbadbc1fa3d71 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4c42434479a12557d713f84f3351880a.js"
+  "/precache-manifest.b0d42a4d26a11b2b86bdd27dc043f552.js"
 );
 
 self.addEventListener('message', (event) => {
