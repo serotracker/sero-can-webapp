77b953907e048a94891ae63057fa7fc7532b6da8
SeanKennyNF <86806388+SeanKennyNF@users.noreply.github.com>
2025-07-28 04:13:20 +0000

    Deploying to gh-pages from  @ aadfd2fe5c4c33ee467960401cef98d48daaaa84 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.32c9da729b6c59fbb45cfff757bd71c3.js"
+  "/precache-manifest.c00fe192d32e3a8c65eb28386dc91b44.js"
 );
 
 self.addEventListener('message', (event) => {
